cdd682b6e20bab46e66d255e5f4eb7e4c8ed88d9
Belkovskii Denis <belkovsky.denis@yandex.ru>
2024-08-29 21:47:58 +0300

    data layer first realization

--- app/src/main/java/com/example/android_view_edu/domain/GetItemUseCase.kt
+++ app/src/main/java/com/example/android_view_edu/domain/GetItemUseCase.kt
@@ -2,6 +2,6 @@ package com.example.android_view_edu.domain
 
 class GetItemUseCase(private val repository : ShopListRepository) {
 
-    fun getShopItem(id : Int) : ShopItem = repository.getShopItem(id)
+    fun getShopItem(id : Int) : ShopItem? = repository.getShopItem(id)
 
 }
